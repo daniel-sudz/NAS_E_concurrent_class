f9cd1e838a9cac23447296eec53f434231e86b1c
Daniel Sudzilouski <dsudzilouski@olin.edu>
2024-06-09 20:29:37 -0700

    fix java bugs

--- week1/intro.java
+++ week1/intro.java
@@ -1,38 +1,38 @@
 class Foo {
-    int order = 1; 
+    volatile int order = 1; 
     public Foo() {
         
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-        order++;
+        synchronized (this) {
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            order++;
+            notifyAll();
+        }
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        
-        // printSecond.run() outputs "second". Do not change or remove this line.
         synchronized (this) {
             while(order != 2) {
-                notify();
+                notifyAll();
                 wait();
             }
+            // printSecond.run() outputs "second". Do not change or remove this line.
             printSecond.run();
             order++;
-            notify();
+            notifyAll();
         }
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        
-        // printThird.run() outputs "third". Do not change or remove this line.
         synchronized (this) {
             while(order != 3) {
-                notify();
+                notifyAll();
                 wait();
             }
+            // printThird.run() outputs "third". Do not change or remove this line.
             printThird.run();
         }
     }

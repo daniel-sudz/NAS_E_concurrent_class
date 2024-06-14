b85bfac47948eb2e0118a6ee5a440da1452ade41
Daniel Sudzilouski <dsudzilouski@olin.edu>
2024-06-13 22:38:07 -0700

    Update intro.java

--- week1/intro.java
+++ week1/intro.java
@@ -1,38 +1,30 @@
 class Foo {
-    volatile int order = 1; 
+    int order = 1; 
     public Foo() {
         
     }
 
-    public void first(Runnable printFirst) throws InterruptedException {
+    public void printn(Runnable printFunc, int n) throws InterruptedException {
         synchronized (this) {
-            // printFirst.run() outputs "first". Do not change or remove this line.
-            printFirst.run();
+            while(order != n) {
+                wait();
+            }
+        }
+        printFunc.run();
+        synchronized (this) {
             order++;
             notifyAll();
         }
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        printn(printFirst, 1);
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        synchronized (this) {
-            while(order != 2) {
-                notifyAll();
-                wait();
-            }
-            // printSecond.run() outputs "second". Do not change or remove this line.
-            printSecond.run();
-            order++;
-            notifyAll();
-        }
+        printn(printSecond, 2);
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        synchronized (this) {
-            while(order != 3) {
-                notifyAll();
-                wait();
-            }
-            // printThird.run() outputs "third". Do not change or remove this line.
-            printThird.run();
-        }
+        printn(printThird, 3);
     }
